ef8cbfa066f50ab5aba104f9b0dd30c15c2f830a
Jaipal <jaipalkumar1293@gmail.com>
2019-04-28 07:57:39 +0500

    search bar configured

--- routes/tutor.js
+++ routes/tutor.js
@@ -5,7 +5,9 @@ const router = require('express').Router(),
       Tutor = require('../models/tutor'),
       Field = require("../models/field"),
       User = require('../models/user'),
-      middleware = require('../middleware')
+      middleware = require('../middleware'),
+      async = require('async'),
+      Message = require('../models/message')
 
 router.get('/info',middleware.isTutorLoggedIn,function(req,res){
     res.render('tutor/info',{user:req.user})
@@ -44,20 +46,9 @@ router.post('/info',middleware.isTutorLoggedIn,(req,res)=>{
             res.redirect('/tutor/add/subject')
         }
     })
-    // Tutor.create(req.body.tutor,(err,tutor)=>{
-    //     if(err){
-    //         console.log(err)
-    //         req.flash('error',err)
-    //         res.redirect('/tutor/info')
-    //     }else{
-    //         res.redirect('/tutor/add/subject')
-    //     }
-    // })
 })
 
 router.get('/add/subject',middleware.isTutorLoggedIn,(req,res)=>{
-    // show fields in subjects
-    //reference them in tutor field
     Field.find({},(err,field)=>{
         if(err){
             console.log(err)
@@ -92,6 +83,224 @@ router.post('/add/subject',middleware.isTutorLoggedIn,(req,res)=>{
             res.redirect('/')
         }
     })
+})
+
+router.get('/search',middleware.isLoggedIn,(req,res)=>{
+    res.redirect('/tutor')
+})
+
+router.get('/results',middleware.isLoggedIn,(req,res)=>{
+    res.redirect('/tutor')
+})
+
+
+router.get('/',middleware.isLoggedIn,(req,res)=>{
+    async.parallel([
+        function(callback){
+            User.find({'status':'tutor'},(err,problem)=>{
+                    callback(err,problem)
+            })
+        },
+
+        function(callback){
+            User.aggregate([
+                {$group: {_id: "$subjects"}} // check later
+            ],(err,result)=>{
+                callback(err,result)
+            })
+        },
+
+        function(callback){
+            User.findOne({'username':req.user.username}).populate('request.userId').exec((err,result)=>{
+                callback(err,result)
+            })
+        },
+        function(callback){
+            const nameRegex = new RegExp("^"+req.user.username.toLowerCase(), 'i')
+
+           Message.aggregate([
+               {$match: {$or:[{'senderName':nameRegex},{'receiverName':nameRegex}]}},
+               {$sort:{'createdAt':-1}},
+               {   $group: {"_id":{
+                       "last_message_between":{
+                           $cond:[
+                               {
+                                   $gt: [
+                                       {$substr:["$senderName",0,1]},
+                                       {$substr:["$receiverName",0,1]}
+                                   ]
+                               },
+                               {$concat: ["$senderName"," and ","$receiverName"]},
+                               {$concat: ["$receiverName"," and ","$senderName"]}
+
+                           ]
+                       }
+                   }, "body":{$first:"$$ROOT"}
+               }
+               }],function(err,newResult){
+                   callback(err,newResult);
+               }
+           )
+       }
+
+    ],(err,problem)=>{
+        if(err){
+            console.log(err)
+        }
+        const tut = problem[0] //callbacks using indexes //instead of tutor push questions 
+        const result2 = problem[1]; //second callback sinces index starts from 0 
+        const result3 = problem[2];
+        const result4 = problem[3];
+        //console.log(result2)
+
+        const dataChunk = []
+        const chunkSize = 3;
+        for (let i = 0 ; i < tut.length;i += chunkSize ){
+            dataChunk.push(tut.slice(i,chunkSize))
+        }
+        res.render('tutor/index',{title:'Online Tutor Bot - Tutors',chunks:dataChunk,fields:result2,user:req.user,data:result3,chat: result4})                
+    })
+})
+
+router.post('/search',middleware.isLoggedIn,(req,res)=>{
+    async.parallel([
+        function(callback){
+            const regex = new RegExp((req.body.tutorsearch),'gi') //tutor
+            User.find({'username':regex,'status':'tutor'},(err,foundTutor)=>{
+                callback(err,foundTutor)
+            })
+        },
+        function(callback){
+            User.aggregate([
+                {$group: {_id: "$subjects"}} // check later
+            ],(err,result)=>{
+                callback(err,result)
+            })
+        },
+
+        function(callback){
+            User.findOne({'username':req.user.username}).populate('request.userId').exec((err,result)=>{
+                callback(err,result)
+            })
+        },
+        function(callback){
+            const nameRegex = new RegExp("^"+req.user.username.toLowerCase(), 'i')
+
+           Message.aggregate([
+               {$match: {$or:[{'senderName':nameRegex},{'receiverName':nameRegex}]}},
+               {$sort:{'createdAt':-1}},
+               {   $group: {"_id":{
+                       "last_message_between":{
+                           $cond:[
+                               {
+                                   $gt: [
+                                       {$substr:["$senderName",0,1]},
+                                       {$substr:["$receiverName",0,1]}
+                                   ]
+                               },
+                               {$concat: ["$senderName"," and ","$receiverName"]},
+                               {$concat: ["$receiverName"," and ","$senderName"]}
+
+                           ]
+                       }
+                   }, "body":{$first:"$$ROOT"}
+               }
+               }],function(err,newResult){
+                   callback(err,newResult);
+               }
+           )
+       }
+
+    ],(err,results)=>{
+        if(err){
+            console.log(err)
+        }
+        const tut = results[0] //callbacks using indexes //instead of tutor push questions 
+        const result2 = results[1]; //second callback sinces index starts from 0 
+        const result3 = results[2];
+        const result4 = results[3];
+        //console.log(result2)
+
+        const dataChunk = []
+        const chunkSize = 3;
+        for (let i = 0 ; i < tut.length;i += chunkSize ){
+            dataChunk.push(tut.slice(i,chunkSize))
+        }
+        res.render('tutor/index',{title:'Online Tutor Bot - Tutors',chunks:dataChunk,fields:result2,user:req.user,data:result3,chat: result4})                
+    })
+
+})
+
+
+
+router.post("/results",(req,res)=>{
+    async.parallel([
+        function(callback){
+            const regex = new RegExp((req.body.country),'gi') //tutor
+            User.find({'subjects':regex,'status':'tutor'},(err,foundTutor)=>{
+                console.log(foundTutor)
+                callback(err,foundTutor)
+            })
+        },
+        function(callback){
+            User.aggregate([
+                {$group: {_id: "$subjects"}} // check later
+            ],(err,result)=>{
+                callback(err,result)
+            })
+        },
+
+        function(callback){
+            User.findOne({'username':req.user.username}).populate('request.userId').exec((err,result)=>{
+                callback(err,result)
+            })
+        },
+        function(callback){
+            const nameRegex = new RegExp("^"+req.user.username.toLowerCase(), 'i')
+
+           Message.aggregate([
+               {$match: {$or:[{'senderName':nameRegex},{'receiverName':nameRegex}]}},
+               {$sort:{'createdAt':-1}},
+               {   $group: {"_id":{
+                       "last_message_between":{
+                           $cond:[
+                               {
+                                   $gt: [
+                                       {$substr:["$senderName",0,1]},
+                                       {$substr:["$receiverName",0,1]}
+                                   ]
+                               },
+                               {$concat: ["$senderName"," and ","$receiverName"]},
+                               {$concat: ["$receiverName"," and ","$senderName"]}
+
+                           ]
+                       }
+                   }, "body":{$first:"$$ROOT"}
+               }
+               }],function(err,newResult){
+                   callback(err,newResult);
+               }
+           )
+       }
+
+    ],(err,results)=>{
+        if(err){
+            console.log(err)
+        }
+        const tut = results[0] //callbacks using indexes //instead of tutor push questions 
+        const result2 = results[1]; //second callback sinces index starts from 0 
+        const result3 = results[2];
+        const result4 = results[3];
+        //console.log(result2)
+
+        const dataChunk = []
+        const chunkSize = 3;
+        for (let i = 0 ; i < tut.length;i += chunkSize ){
+            dataChunk.push(tut.slice(i,chunkSize))
+        }
+        res.render('tutor/index',{title:'Online Tutor Bot - Tutors',chunks:dataChunk,fields:result2,user:req.user,data:result3,chat: result4})                
+    })
+
 })
 
 module.exports = router
